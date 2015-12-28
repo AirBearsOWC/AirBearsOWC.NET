bbd2b709d243fa1cecc3b779007e1c4af6e7bd48
Tom Faltesek <tfaltesek@intertech.com>
2015-12-28 11:25:05 -0600

    Wired up registration options.

--- src/AirBears.Web/wwwroot/app/registration/register-options-modal.controller.js
+++ src/AirBears.Web/wwwroot/app/registration/register-options-modal.controller.js
@@ -5,16 +5,22 @@
         .module("app")
         .controller("RegisterOptionsModalController", RegisterOptionsModalController);
 
-    RegisterOptionsModalController.$inject = ["$uibModalInstance"];
+    RegisterOptionsModalController.$inject = ["$uibModalInstance", "$state"];
 
-    function RegisterOptionsModalController($uibModalInstance) {
+    function RegisterOptionsModalController($uibModalInstance, $state) {
         var vm = this;
 
+        vm.navigatTo = navigatTo;
         vm.cancel = cancel;
 
         activate();
 
         function activate() { }
+
+        function navigatTo(state) {
+            $state.go(state);
+            $uibModalInstance.dismiss("cancel");
+        }
 
         function cancel() {
             $uibModalInstance.dismiss("cancel");
