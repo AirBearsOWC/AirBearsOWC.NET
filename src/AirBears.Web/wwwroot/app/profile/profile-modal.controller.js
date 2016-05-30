43ea2e4bd6bcab02c1bfeb342e300133dccad49f
Tom Faltesek <tfaltesek@intertech.com>
2016-05-29 23:55:47 -0500

    Fixed a typo. Navigating to profile page closes modal window.

--- src/AirBears.Web/wwwroot/app/profile/profile-modal.controller.js
+++ src/AirBears.Web/wwwroot/app/profile/profile-modal.controller.js
@@ -5,17 +5,24 @@
         .module("app")
         .controller("ProfileModalController", ProfileModalController);
 
-    ProfileModalController.$inject = ["$uibModalInstance", "pilot"];
+    ProfileModalController.$inject = ["$uibModalInstance", "$state", "pilot"];
 
-    function ProfileModalController($uibModalInstance, pilot) {
+    function ProfileModalController($uibModalInstance, $state, pilot) {
         var vm = this;
 
+        vm.openProfilePage = openProfilePage;
         vm.cancel = cancel;
 
         activate();
 
         function activate() {
             vm.pilot = pilot;
+        }
+
+        function openProfilePage()
+        {
+            $state.go("root.profile", { pilotId: vm.pilot.id, pilot: vm.pilot });
+            $uibModalInstance.dismiss("cancel");
         }
 
         function cancel() {
